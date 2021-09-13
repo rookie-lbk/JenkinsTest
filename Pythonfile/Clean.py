f70fb1fb233b646fba4ad994e51cfe3e4fec96a8
liubenkang <benkang.liu@centurygame.com>
2021-09-13 18:51:03 +0800

    no message

---
+++ Pythonfile/Clean.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+import os
+import json
+
+import sys
+reload(sys)
+
+sys.setdefaultencoding('utf8')
+currentDir = os.path.split(os.path.realpath(__file__))[0]
+
+def OnStart():
+	os.chdir(currentDir + "/../")
+	command = "git clean -d -f"
+	os.system(command)
+	command = "git reset --hard"
+	os.system(command)
+try:
+	OnStart()
+except Exception as e:
+	print(e)
+else:
+	pass
+finally:
+	pass
+
